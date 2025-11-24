df91f3b31f05db794b07ab4567bceaf6b5f149d5
Azi Juniar <103876707+zeekuu@users.noreply.github.com>
2025-11-25 05:56:53 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+function lightMode() {
+    document.body.classList.remove("dark")
+}
 
+function darkMode(){
+    document.body.classList.add("dark")
+}
+
+function validasiForm() {
+  let nt = document.getElementById("namaTugas").value;
+  let mk = document.getElementById("mataKuliah").value;
+  let dead = document.getElementById("deadline").value;
+
+  if (nt === ""){
+    alert("Nama Tugas Harus Di isi");
+    return false;
+  }
+  if (mk === ""){
+    alert("Mata Kuliah Harus Di isi");
+    return false;
+  }
+  if (dead === ""){
+    alert("Deadline Harus Di isi");
+    return false;
+  }
+  alert("Tugas Berhasil Dicatat");
+  return true;
+}
